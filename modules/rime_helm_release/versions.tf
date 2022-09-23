3b44871f524079d1230ad13346564eab9fb3232d
Robust Intelligence <ri-hlin@users.noreply.github.com>
2022-09-23 23:37:16 +0000

    Automated Terraform module publish for 0.19.0-rc.5 on 20220923_233716

--- modules/rime_helm_release/versions.tf
+++ modules/rime_helm_release/versions.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = "> 0.14, < 2.0.0"
+  // Adds `optional` as a type constraint for variable declations.
+  // This feature was added experimentally in 0.14.0 (2020-12-02):
+  //   https://github.com/hashicorp/terraform/releases/tag/v0.14.0
+  // This feature became non-experimental in 1.3.0 (21-09-2022):
+  //   https://github.com/hashicorp/terraform/releases/tag/v1.3.0
+  experiments      = [module_variable_optional_attrs]
 
   required_providers {
     helm = {
