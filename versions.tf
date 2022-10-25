97b7abee8937c4c8ce066fdb8c1fb1b00fee34ff
Robust Intelligence <ri-hlin@users.noreply.github.com>
2022-10-25 07:16:37 +0000

    Automated Terraform module publish for 0.19.6 on 20221025_071637

--- versions.tf
+++ versions.tf
@@ -26,5 +26,12 @@ terraform {
       source  = "hashicorp/random"
       version = "3.0.0"
     }
+
+    helm = {
+      source = "hashicorp/helm"
+      # Note: Do not use 2.0.0 or 2.0.1 - these versions are buggy.
+      # See: https://github.com/hashicorp/terraform-provider-helm/issues/662
+      version = "> 2.1.0, < 3.0.0"
+    }
   }
 }
