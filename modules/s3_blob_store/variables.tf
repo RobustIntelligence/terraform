a134c5bfba5c6f782f2ca9d9022664e6209d0cff
Robust Intelligence <ri-hlin@users.noreply.github.com>
2023-07-27 02:39:51 +0000

    Automated Terraform module publish for 0.20.25-rc.0 on 20230727_023951

---
+++ modules/s3_blob_store/variables.tf
@@ -0,0 +1,45 @@
+variable "use_blob_store" {
+  description = "Whether to use blob store for the cluster."
+  type        = bool
+  default     = true
+}
+
+variable "k8s_namespace" {
+  description = "Namespace where the RIME Helm chart is installed."
+  type = object({
+    namespace = string
+    primary   = bool
+  })
+  # This module requires that these conditions are met.
+  # The validation conditions should match the ones in the outer most level where k8s_namespace string is first passed as input.
+  # Redundant validation is added here as a safeguard for when the outer most k8s_namespace conidtion is editted without updating this condition.
+  # The conditions are required because k8s_namespace is included in the blob-store S3 bucket name, which has a limit on length and what characters can be included.
+  validation {
+    condition     = length(var.k8s_namespace.namespace) <= 12 && can(regex("^[a-z0-9.-]+$", var.k8s_namespace.namespace))
+    error_message = "Must not be longer than 12 characters and must contain only letters, numbers, dots (.), and hyphens (-)."
+  }
+}
+
+variable "oidc_provider_url" {
+  description = "URL to the OIDC provider for IAM assumable roles used by K8s."
+  type        = string
+}
+
+variable "resource_name_suffix" {
+  description = "A suffix to name the IAM policy and role with."
+  type        = string
+  # This module requires that these conditions are met.
+  # The validation conditions should match the one in the outer most level where resource_name_suffix is first passed as input.
+  # Redundant validation is added here as a safeguard for when the outer most resource_name_suffix conidtion is editted without updating this condition.
+  # The conditions are required because resource_name_suffix is included in the blob-store S3 bucket name, which has a limit on length and what characters can be included.
+  validation {
+    condition     = length(var.resource_name_suffix) <= 25 && can(regex("^[a-z0-9.-]+$", var.resource_name_suffix))
+    error_message = "Must not be longer than 25 characters and must contain only letters, numbers, dots (.), and hyphens (-)."
+  }
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources."
+  type        = map(string)
+  default     = {}
+}
---
+++ modules/s3_iam/main.tf
@@ -0,0 +1,87 @@
+# This policy depends on the set of S3 paths that our service needs access to
+# supplied by the inputs to our terraform module.
+#
+# For an example of such a policy;
+# see: https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_condition_operators.html#Conditions_String
+#
+# For specification of the bucket ARNs;
+# see: https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_resource.html
+data "aws_iam_policy_document" "eks_s3_access_policy_document" {
+  # Add a policy statement per bucket-path that allows one to list objects within
+  # that bucket-path.
+  dynamic "statement" {
+    for_each = var.s3_authorized_bucket_path_arns
+    content {
+      actions = [
+        "s3:ListBucket",
+      ]
+
+      # The resource that we're allowing list access to is an S3 bucket (no subdirectories).
+      # This regexp matches the bucket name of the form:
+      #     arn:aws:s3:::${BUCKET_NAME}
+      # where ${BUCKET_NAME} has no '/' separators.
+      resources = [
+        replace(statement.value, "/^(arn:aws:s3:::[^/]+)(?:/.*)?$/", "$1"),
+      ]
+
+      # This restricts the list access to only paths matching the given parent directory;
+      # ie. if the full bucket path was 'arn:aws:s3:::${BUCKET_NAME}/foo/bar/baz/*'
+      # then we want to resrict the list access to only paths that begin with '/foo/bar/baz'
+      dynamic "condition" {
+        # Only create this prefix if the given path includes at least 1 parent directory.
+        # TODO(blaine): This is a hack to use a for_each to do a conditional.
+        for_each = length(regexall("^arn:aws:s3:::[^/]+/[^/]+/.*$", statement.value)) > 0 ? ["dummy_value"] : []
+        content {
+          test     = "StringLike"
+          variable = "s3:prefix"
+
+          # Extract the parent directory.
+          values = [
+            "${dirname(replace(statement.value, "/^arn:aws:s3:::[^/]+/(.*)$/", "$1"))}/*",
+          ]
+        }
+      }
+    }
+  }
+
+  # Add a policy statement that allows one to get (read) objects in all
+  # of the allowed bucket-paths.
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = var.s3_authorized_bucket_path_arns
+  }
+}
+
+resource "aws_iam_policy" "eks_s3_access_policy" {
+  name = "eks_s3_access_policy_${var.resource_name_suffix}_${var.k8s_namespace.namespace}"
+
+  policy = data.aws_iam_policy_document.eks_s3_access_policy_document.json
+
+  tags = var.tags
+}
+
+module "iam_assumable_role_with_oidc_for_s3_access" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "~> 3.0"
+
+  create_role      = true
+  role_name        = "eks_s3_access_${var.resource_name_suffix}_${var.k8s_namespace.namespace}"
+  role_description = "Role to access s3 bucket"
+
+  provider_url = var.oidc_provider_url
+
+  role_policy_arns = [
+    aws_iam_policy.eks_s3_access_policy.arn,
+  ]
+
+  number_of_role_policy_arns = 1
+
+  oidc_fully_qualified_subjects = [
+    "system:serviceaccount:${var.k8s_namespace.namespace}:${var.service_account_name}"
+  ]
+
+  tags = var.tags
+}
