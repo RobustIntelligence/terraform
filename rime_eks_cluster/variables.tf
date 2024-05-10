c473bf9628aa4d0712caf7f8aaf0826f00ce9128
Robust Intelligence <action@github.com>
2024-05-10 04:06:52 +0000

    Automated Terraform module publish for 2.8.0-rc.13 on 20240510_040652

--- rime_eks_cluster/variables.tf
+++ rime_eks_cluster/variables.tf
@@ -6,7 +6,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version to use for the EKS cluster."
   type        = string
-  default     = "1.23"
+  default     = "1.25"
 }
 
 variable "public_cluster_endpoint" {
@@ -52,7 +52,7 @@ variable "map_users" {
 variable "model_testing_worker_group_instance_types" {
   description = "Instance types for the model testing worker group."
   type        = list(string)
-  default     = ["t2.xlarge", "t3.xlarge", "t3a.xlarge"]
+  default     = ["t3.xlarge", "t2.xlarge"]
 
   validation {
     condition     = length(var.model_testing_worker_group_instance_types) >= 1
@@ -61,7 +61,7 @@ variable "model_testing_worker_group_instance_types" {
 }
 
 variable "model_testing_worker_group_min_size" {
-  description = "Minimum size of the model testing worker group. Must be >= 1"
+  description = "Minimum size of the model testing worker group. Must be >= 0"
   type        = number
   default     = 0
 
@@ -126,6 +126,68 @@ variable "model_testing_worker_group_use_spot" {
   default     = true
 }
 
+variable "model_testing_worker_group_root_volume_size" {
+  description = "Root volume size in GB for the model testing worker group."
+  type        = number
+  default     = 100
+}
+
+variable "model_testing_worker_group_large_instance_types" {
+  description = "Instance types for the large model testing worker group."
+  type        = list(string)
+  default     = ["m5.12xlarge", "m5a.12xlarge", "m5n.12xlarge", "m6i.12xlarge", "m6a.12xlarge", "m7i.12xlarge"]
+  validation {
+    condition     = length(var.model_testing_worker_group_large_instance_types) >= 1
+    error_message = "Must specify at least one instance type."
+  }
+}
+
+variable "model_testing_worker_group_large_min_size" {
+  description = "Minimum size of the large model testing worker group. Must be >= 0"
+  type        = number
+  default     = 0
+
+  validation {
+    condition     = var.model_testing_worker_group_large_min_size >= 0
+    error_message = "Large model testing worker group min size must be greater than or equal to 0."
+  }
+}
+
+variable "model_testing_worker_group_large_desired_size" {
+  description = <<EOT
+  Desired size of the large model testing worker group.
+  If var.use_managed_node_group is true, must be >= 1; otherwise, must be >= 0.
+  EOT
+  type        = number
+  default     = 0
+
+  validation {
+    condition     = var.model_testing_worker_group_large_desired_size >= 0
+    error_message = <<EOT
+    Large model testing worker group desired size must be greater than or equal to 0.
+    If var.use_managed_node_group is true, must be >= 1.
+    EOT
+  }
+}
+
+variable "model_testing_worker_group_large_max_size" {
+  description = "Maximum size of the large model testing worker group. Must be >= min size. For best performance we recommend >= 10 nodes as the max size."
+  type        = number
+  default     = 10
+}
+
+variable "model_testing_worker_group_large_root_volume_size" {
+  description = "Root volume size in GB for the large model testing worker group."
+  type        = number
+  default     = 100
+}
+
+variable "model_testing_worker_groups_large_overrides" {
+  description = "A dictionary that specifies overrides for the large model testing worker group launch templates. See https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v17.24.0/locals.tf#L36 for valid values."
+  type        = any
+  default     = {}
+}
+
 variable "private_subnet_ids" {
   description = "A list of private subnet ids to place the EKS cluster and workers within. Must be specified if create_eks is true"
   type        = list(string)
@@ -141,7 +203,7 @@ variable "public_subnet_ids" {
 variable "server_worker_group_instance_types" {
   description = "Instance types for the server worker group."
   type        = list(string)
-  default     = ["t2.xlarge"]
+  default     = ["t3.xlarge"]
 
   validation {
     condition     = length(var.server_worker_group_instance_types) >= 1
@@ -152,7 +214,7 @@ variable "server_worker_group_instance_types" {
 variable "server_worker_group_min_size" {
   description = "Minimum size of the server worker group. Must be >= 1"
   type        = number
-  default     = 4
+  default     = 2
 
   validation {
     condition     = var.server_worker_group_min_size >= 1
@@ -163,7 +225,7 @@ variable "server_worker_group_min_size" {
 variable "server_worker_group_desired_size" {
   description = "Desired size of the server worker group. Must be >= 0"
   type        = number
-  default     = 5
+  default     = 4
 
   validation {
     condition     = var.server_worker_group_desired_size >= 0
@@ -176,6 +238,13 @@ variable "server_worker_group_max_size" {
   type        = number
   default     = 10
 }
+
+variable "server_worker_group_root_volume_size" {
+  description = "Root volume size in GB for the large server worker group."
+  type        = number
+  default     = 100
+}
+
 
 variable "server_worker_groups_overrides" {
   description = "A dictionary that specifies overrides for the server worker group launch templates. See https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v17.24.0/locals.tf#L36 for valid values."
@@ -203,4 +272,28 @@ variable "use_managed_node_group" {
   EOT
   type        = bool
   default     = false
+}
+
+variable "enable_cni_network_policy" {
+  description = "Boolen to enable network policy on the cluster. The aws cni plugin requires a min k8s version of 1.25 to enable this."
+  type        = bool
+  default     = false
+}
+
+variable "cluster_enabled_log_types" {
+  description = "The list of log types to enable for the cluster. By default, all log types are enabled."
+  type        = list(string)
+  default = [
+    "api",
+    "audit",
+    "authenticator",
+    "controllerManager",
+    "scheduler"
+  ]
+}
+
+variable "cluster_log_retention_in_days" {
+  description = "Number of days to retain log events. Default retention - 90 days."
+  type        = number
+  default     = 90
 }
