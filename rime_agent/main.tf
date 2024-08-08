3c3018fbb445f166de5ca70ac436e90aeeec8442
Robust Intelligence <action@github.com>
2024-08-08 00:54:53 +0000

    Automated Terraform module publish for 2.9.9 on 20240808_005453

--- rime_agent/main.tf
+++ rime_agent/main.tf
@@ -2,9 +2,21 @@ locals {
   tags       = merge({ ManagedBy = "Terraform" }, var.tags)
   output_dir = length(var.helm_values_output_dir) == 0 ? path.cwd : var.helm_values_output_dir
 
+  # Service account names are used in both the helm chart to create the service account and s3_iam module to link S3 access via OIDC.
   # the service account name for a running model test job.
-  # This name is used in both the helm chart to create the service account and s3_iam module to link S3 access via OIDC.
   model_test_job_service_account_name = "rime-agent-model-tester"
+  # the service account name for the cross plane server.
+  cross_plane_server_service_account_name = "rime-agent-cross-plane-server"
+  # the service account name for the file server.
+  file_server_service_account_name = "rime-agent-file-server"
+  # the service account name for the generative validation service.
+  generative_model_testing_service_account_name = "rime-agent-generative-model-tester"
+
+
+
+  # name of the signing key secret.
+  # this is registered and stored in the CP and used to sign JWTs for auth.
+  signing_key_secret_name = "rime-agent-signing-key-terraform"
 }
 
 // Creates S3 reader roles to be used by model testing jobs
@@ -17,9 +29,40 @@ module "s3_iam" {
   oidc_provider_url              = var.oidc_provider_url
   resource_name_suffix           = "${var.resource_name_suffix}_agent"
   s3_authorized_bucket_path_arns = var.s3_authorized_bucket_path_arns
-  service_account_name           = local.model_test_job_service_account_name
+  service_account_names = [
+    local.model_test_job_service_account_name,
+    local.cross_plane_server_service_account_name,
+  ]
 
   tags = local.tags
+}
+
+module "bedrock" {
+  source = "./bedrock"
+
+  namespace            = var.namespace
+  oidc_provider_url    = var.oidc_provider_url
+  resource_name_suffix = "${var.resource_name_suffix}_agent"
+  service_account_names = [
+    local.generative_model_testing_service_account_name
+  ]
+
+  tags = local.tags
+}
+
+
+// Create blob store bucket and IAM permissions to upload to the blob store.
+module "blob_store" {
+  source = "../rime_helm_release/blob_store"
+
+  count = var.enable_blob_store ? 1 : 0
+
+  namespace             = var.namespace
+  oidc_provider_url     = var.oidc_provider_url
+  resource_name_suffix  = "agent-${var.resource_name_suffix}"
+  service_account_names = [local.file_server_service_account_name]
+  force_destroy         = true
+  tags                  = var.tags
 }
 
 // Create namespace, only if enabled.
@@ -30,6 +73,22 @@ resource "kubernetes_namespace" "auto" {
     labels = {
       name = var.namespace
     }
+  }
+}
+
+// Create cross service key secret with terraform so it can be consistent
+// when we helm uninstall the agent.
+resource "random_password" "internal_agent_generated_api_key" {
+  length = 16
+}
+
+resource "kubernetes_secret" "signing_key_secret" {
+  metadata {
+    name      = local.signing_key_secret_name
+    namespace = var.namespace
+  }
+  data = {
+    crossServiceKey = random_password.internal_agent_generated_api_key.result
   }
 }
 
@@ -55,30 +114,68 @@ resource "kubernetes_secret" "docker-secrets" {
   type       = "kubernetes.io/dockerconfigjson"
 }
 
+// Create a secret containing integrations for generative model testing.
+resource "kubernetes_secret" "generative-model-testing-secrets" {
+  count = var.generative_model_testing_config.enable ? 1 : 0
+
+  metadata {
+    name      = "rime-agent-generative-model-testing-secrets"
+    namespace = var.namespace
+  }
+
+  data = {
+    huggingfaceAPIKey = var.generative_model_testing_config.huggingface_api_key
+  }
+  depends_on = [kubernetes_namespace.auto]
+}
 
 resource "local_file" "terraform_provided_values" {
   content = templatefile("${path.module}/values_tmpl.yaml", {
-    image                               = var.rime_docker_agent_image
-    version                             = var.rime_version
-    default_rime_engine_image           = var.rime_docker_default_engine_image
-    s3_reader_role_arn                  = module.s3_iam.s3_reader_role_arn
-    docker_registry                     = var.docker_registry
-    image_pull_secret_name              = var.docker_secret_name
-    enable_crossplane_tls               = var.enable_crossplane_tls
-    enable_cert_manager                 = var.enable_cert_manager
-    agent_manager_server_addr           = "${var.cp_release_name}-agent-manager-server.${var.cp_namespace}:5016"
-    upload_server_addr                  = "${var.cp_release_name}-upload-server.${var.cp_namespace}:5000"
-    firewall_server_rest_addr           = "${var.cp_release_name}-firewall-server.${var.cp_namespace}:15002"
-    data_collector_rest_addr            = "${var.cp_release_name}-data-collector-server.${var.cp_namespace}:15015"
-    upload_server_rest_addr             = "${var.cp_release_name}-upload-server.${var.cp_namespace}:15001"
-    dataset_manager_server_rest_addr    = "${var.cp_release_name}-dataset-manager-server.${var.cp_namespace}:15009"
-    model_test_job_service_account_name = local.model_test_job_service_account_name
-    datadog_tag_pod_annotation          = var.datadog_tag_pod_annotation
+    agent_id                         = var.agent_id
+    existing_api_key_secret_name     = var.existing_api_key_secret_name
+    existing_signing_key_secret_name = kubernetes_secret.signing_key_secret.metadata[0].name
+    image                            = var.rime_docker_agent_image
+    version                          = var.rime_version
+    default_rime_engine_image        = var.rime_docker_default_engine_image
+    s3_reader_role_arn               = module.s3_iam.s3_reader_role_arn
+    bedrock_access_role_arn          = module.bedrock.bedrock_access_role_arn
+    docker_registry                  = var.docker_registry
+    image_pull_secret_name           = var.docker_secret_name
+    enable_crossplane_tls            = var.enable_crossplane_tls
+    enable_cert_manager              = var.enable_cert_manager
+    enable_support_bundle            = var.enable_support_bundle
+
+    generative_model_testing_config = {
+      enable                 = var.generative_model_testing_config.enable
+      secret_name            = var.generative_model_testing_config.enable ? kubernetes_secret.generative-model-testing-secrets[0].metadata[0].name : ""
+      detection_engine_image = var.generative_model_testing_config.rime_docker_detection_engine_image
+      model_server_image     = var.generative_model_testing_config.rime_docker_model_server_image
+      firewall_backend_image = var.generative_model_testing_config.rime_docker_firewall_backend_image
+      firewall_version       = var.generative_model_testing_config.rime_docker_firewall_image_version
+    }
+
+    # Address of the CP NGINX ingress controller service for the internal
+    # agent to communicate with.
+    cp_nginx_controller_rest_addr = "${var.cp_release_name}-ingress-nginx-controller"
+
+    model_test_job_service_account_name           = local.model_test_job_service_account_name
+    cross_plane_server_service_account_name       = local.cross_plane_server_service_account_name
+    generative_model_testing_service_account_name = local.generative_model_testing_service_account_name
+    datadog_tag_pod_annotation                    = var.datadog_tag_pod_annotation
     log_archival_config = {
       enable      = var.log_archival_config.enable
       bucket_name = var.log_archival_config.bucket_name
+      endpoint    = var.log_archival_config.endpoint
+      type        = var.log_archival_config.type
       role_arn    = var.log_archival_config.enable ? module.iam_assumable_role_with_oidc_for_log_archival[0].this_iam_role_arn : ""
     }
+
+    blob_store_config = {
+      enable      = var.enable_blob_store
+      bucket_name = var.enable_blob_store ? module.blob_store[0].blob_store_bucket_name : ""
+      role_arn    = var.enable_blob_store ? module.blob_store[0].blob_store_role_arn : ""
+    }
+
     separate_model_testing_group = var.separate_model_testing_group
   })
   filename = format("%s/rime_agent_values_terraform_%s.yaml", local.output_dir, var.namespace)
@@ -101,9 +198,12 @@ resource "helm_release" "rime_agent" {
   recreate_pods     = false
   wait              = false
   atomic            = true
+  timeout           = 600
 
   values = [
-    length(var.custom_values_file_path) > 0 ? file(var.custom_values_file_path) : "",
     local_file.terraform_provided_values.content,
+    length(var.override_values_file_path) > 0 ? file(var.override_values_file_path) : "",
   ]
+
+  depends_on = [var.dependency_link]
 }
