3de33f95a4522f030c4db21f24fd84c55f4a2a55
Robust Intelligence <action@github.com>
2024-08-24 00:48:01 +0000

    Automated Terraform module publish for 0.6.8 on 20240824_004801

---
+++ rime_helm_release/security_db_iam/variables.tf
@@ -0,0 +1,24 @@
+variable "namespace" {
+  description = "The k8s namespace to install the RIME cluster into"
+  type        = string
+}
+
+variable "oidc_provider_url" {
+  description = "URL to the OIDC provider for IAM assumable roles used by K8s."
+  type        = string
+}
+
+variable "resource_name_suffix" {
+  description = "A suffix to name the IAM policy and role with."
+  type        = string
+}
+
+variable "service_account_names" {
+  description = "The names of the service accounts to link to the IAM role"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources. Tags added to launch configuration or templates override these values for ASG Tags only."
+  type        = map(string)
+}
