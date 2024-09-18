dd0f619a6b84f54a20065967c9ab73e48fb5b93f
Robust Intelligence <action@github.com>
2024-09-18 00:23:04 +0000

    Automated Terraform module publish for 2.9.18 on 20240918_002304

--- rime_helm_release/outputs.tf
+++ rime_helm_release/outputs.tf
@@ -5,3 +5,7 @@ output "blob_store_bucket_arn" {
 output "blob_store_bucket_name" {
   value = var.enable_blob_store ? module.blob_store[0].blob_store_bucket_name : ""
 }
+
+output "dependency_link" {
+  value = var.create_managed_helm_release ? { namespace = helm_release.rime[0].metadata[0].namespace } : {}
+}
