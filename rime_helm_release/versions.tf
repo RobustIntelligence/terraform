90a412d3846df7ba16b8c5dc7f52eee2c46f339c
Robust Intelligence <action@github.com>
2024-09-24 23:04:02 +0000

    Automated Terraform module publish for 2.11.0-rc.1 on 20240924_230402

--- rime_helm_release/versions.tf
+++ rime_helm_release/versions.tf
@@ -13,6 +13,10 @@ terraform {
       # See: https://github.com/hashicorp/terraform-provider-helm/issues/662
       version = "> 2.1.0, < 3.0.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.75.0"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.1, < 3.0.0"
@@ -20,6 +24,14 @@ terraform {
     local = {
       source  = "hashicorp/local"
       version = ">= 2.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.1"
+    }
+    onepassword = {
+      source  = "1Password/onepassword"
+      version = "2.1.0"
     }
   }
 }
