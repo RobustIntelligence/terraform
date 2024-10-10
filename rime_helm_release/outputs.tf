e6b1747a267a1701d1393ffdd82bbb475c2bf21a
Roee Landesman <roee@robustintelligence.com>
2024-10-10 16:11:16 -0700

    Add internal_firewall_agent_api_key_secret_name output

--- rime_helm_release/outputs.tf
+++ rime_helm_release/outputs.tf
@@ -6,6 +6,10 @@ output "blob_store_bucket_name" {
   value = var.enable_blob_store ? module.blob_store[0].blob_store_bucket_name : ""
 }
 
+output "internal_firewall_agent_api_key_secret_name" {
+  value = local.internal_firewall_agent_api_key_secret_name
+}
+
 output "dependency_link" {
   value = var.create_managed_helm_release ? { namespace = helm_release.rime[0].metadata[0].namespace } : {}
 }
